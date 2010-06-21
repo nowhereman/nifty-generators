0544b7cf7aeff93d39a998bf1a79f22ce2aa3cc6
Nowhere Man <nowhereman@open_office>
2010-06-21 14:45:54 +0200

    nifty_layout: Added a new option 'external_source' to add custom templates. For the moment it's only useful for the application layout.

--- generators/nifty_layout/nifty_layout_generator.rb
+++ generators/nifty_layout/nifty_layout_generator.rb
@@ -15,12 +15,16 @@ class NiftyLayoutGenerator < Rails::Generator::Base
         m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
         m.file     "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
       else
-        m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
+        if options[:external_source] && File.exist?( File.join(options[:external_source], "layout.html.erb") )
+          m.template([ File.join(options[:external_source], "layout.html.erb"), { :full_path => true } ], "app/views/layouts/#{file_name}.html.erb")
+        else
+          m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
+        end
         m.file     "stylesheet.css",  "public/stylesheets/#{file_name}.css"
       end
       if options[:jquery]
         m.file 'application_helper.rb', 'app/helpers/application_helper.rb'# Modified application_helper.rb to be jQuery compatible
-        m.file 'application.js', 'public/javascripts/application.js'# Create application.js to be jQuery compatible
+        m.file 'application.js', 'public/javascripts/application.js'# Create an application.js compatible with jQuery
       end
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
@@ -28,6 +32,43 @@ class NiftyLayoutGenerator < Rails::Generator::Base
 
   def file_name
     @name.underscore
+  end
+
+  # Return the full path from the source root for the given path.
+  # Example for source_root = '/source':
+  #   source_path('some/path.rb') == '/source/some/path.rb'
+  #
+  # Or an external path if the option :full path is set.
+  # Example :
+  #   source_path('/my_external/path.rb', :full_path => true) == '/my_external/path.rb'
+  #
+  # The given path may include a colon ':' character to indicate that
+  # the file belongs to another generator.  This notation allows any
+  # generator to borrow files from another.  Example:
+  #   source_path('model:fixture.yml') = '/model/source/path/fixture.yml'
+  def source_path(relative_source)
+    source_options = {}
+    if relative_source.respond_to?(:extract_options!)
+      source_options = relative_source.extract_options!
+      relative_source = relative_source.first #if relative_source.length == 1
+    end
+
+    # Check whether we're referring to another generator's file.
+    name, path = relative_source.split(':', 2)
+   
+    if source_options[:full_path]
+        relative_source
+
+    # If not, return the full path to our source file if it isn't a full path.
+    elsif path.nil?
+      File.join(source_root, name)
+
+    # Otherwise, ask our referral for the file.
+    else
+      # FIXME: this is broken, though almost always true.  Others'
+      # source_root are not necessarily the templates dir.
+      File.join(self.class.lookup(name).path, 'templates', path)
+    end
   end
 
   protected
@@ -40,6 +81,7 @@ class NiftyLayoutGenerator < Rails::Generator::Base
       opt.on("--haml", "Generate HAML for view, and SASS for stylesheet.") { |v| options[:haml] = v }
       opt.on("--jammit", "Use Jammit, an industrial strength asset packaging.") { |v| options[:jammit] = v }
       opt.on("--jquery", "Use jQuery unobtrusive goodness.") { |v| options[:jquery] = v }
+      opt.on("--external-source=REQUIRED", "Set an external source path to add custom templates.") { |v| options[:external_source] = v }
     end
 
     def banner
